57d9d3626d111de971f995525fadd5ff2626367c
Jason Inouye <jasoninouye@Jasons-MacBook-Pro.local>
2022-07-28 17:01:04 -0500

    object component added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import {ThemeProvider} from '@ui5/webcomponents-react';
 import Home from './components/Home';
+import DetailsPage from './components/DetailsPage';
 
 function App() {
   return (
     <ThemeProvider>
-      <Home />
+      {/* <Home /> */}
+      <DetailsPage />
     </ThemeProvider>
   );
 }
