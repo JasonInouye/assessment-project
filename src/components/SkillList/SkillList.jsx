c13039e94d0b149cc64f842b185b7e8c88c8ddf7
Jason Inouye <jasoninouye@Jasons-MacBook-Pro.local>
2022-08-02 11:35:38 -0500

    added skillList component with reducer data

--- src/components/SkillList/SkillList.jsx
+++ src/components/SkillList/SkillList.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import "@ui5/webcomponents-icons/dist/save";
 import {spacing} from '@ui5/webcomponents-react-base'
@@ -6,31 +6,51 @@ import {
     Button,
     Card,
     CardHeader,
+    Grid,
     RatingIndicator,
     Text
     } from '@ui5/webcomponents-react'
 
 function SkillList() {
     const skills = useSelector((store) => store.skills);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch({ type: 'GET_SKILLS'});
+    }, []);
 
     return (
+
         <div>
-            <Card                
-                header={<CardHeader titleText="SAPUI5" />}
-                style={{ width: "375px", ...spacing.sapUiContentPadding, margin: "30px" }}
-            >
-                <Text style={{ ...spacing.sapUiContentPadding }}>
-                    Build Enterprice-grade web apps with modern, rich user interfaces.
-                </Text>
-
-                <RatingIndicator onChange={function noRefCheck(){}} style={{ ...spacing.sapUiContentPadding }}/>
-
-                <Button
-                    onClick={function noRefCheck(){}}
-                >
-                    Interested
-                </Button>
-            </Card>
+            {skills.map((skillItem) =>{
+                return(
+                    <div key={skillItem.id}>
+                        <Grid>
+                            <div 
+                            data-layout-indent="XL3 L3 M3 S1"
+                            data-layout-span="XL4 L4 M4 S6">                  
+                            <Card                
+                                header={<CardHeader titleText={skillItem.skill} />}
+                                style={{ width: "375px", ...spacing.sapUiContentPadding, margin: "30px" }}
+                            >
+                                <Text style={{ ...spacing.sapUiContentPadding }}>
+                                    {skillItem.description}
+                                </Text>
+                                <div style={{...spacing.sapUiContentPadding}}>
+                                    <RatingIndicator onChange={function noRefCheck(){}} style={{ ...spacing.sapUiContentPadding }}/>
+                                    <Button
+                                        onClick={function noRefCheck(){}}
+                                        style={{marginLeft: "80px" }}
+                                    >
+                                        Interested
+                                    </Button>
+                                </div>
+                            </Card>
+                            </div> 
+                        </Grid>
+                    </div>
+                )
+            })}
         </div>
     )
 }
--- src/redux/reducers/skills.reducer.js
+++ src/redux/reducers/skills.reducer.js
@@ -1,7 +1,71 @@
-const skills = (state = ['SAPUI5','Fiori','React','VUE','ABAP'], action) => { 
+const initialState = [
+    {
+        id: 1,
+        skill: 'SAPUI5',
+        description: 'Build Enterprice-grade web apps with modern, rich user Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget feugiat nulla.'
+    },
+    {
+        id: 2,
+        skill: 'ABAP',
+        description: 'Build Enterprice-grade web apps with modern, rich user Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget feugiat nulla.'
+    },
+    {
+        id: 3,
+        skill: 'React',
+        description: 'Build Enterprice-grade web apps with modern, rich user Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget feugiat nulla.'
+    },
+    {
+        id: 4,
+        skill: 'SAP Fiori',
+        description: 'Build Enterprice-grade web apps with modern, rich user Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget feugiat nulla.'
+    },
+    {
+        id: 5,
+        skill: 'VUE',
+        description: 'Build Enterprice-grade web apps with modern, rich user Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget feugiat nulla.'
+    },
+    {
+        id: 6,
+        skill: 'SAP HANA',
+        description: 'Build Enterprice-grade web apps with modern, rich user Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget feugiat nulla.'
+    },
+    {
+        id: 7,
+        skill: 'SAP Graph',
+        description: 'Build Enterprice-grade web apps with modern, rich user Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget feugiat nulla.'
+    },
+    {
+        id: 8,
+        skill: 'JavaScript',
+        description: 'Build Enterprice-grade web apps with modern, rich user Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget feugiat nulla.'
+    },
+    {
+        id: 9,
+        skill: 'AWS',
+        description: 'Build Enterprice-grade web apps with modern, rich user Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget feugiat nulla.'
+    },
+    {
+        id: 10,
+        skill: 'Figma',
+        description: 'Build Enterprice-grade web apps with modern, rich user Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget feugiat nulla.'
+    },
+    {
+        id: 11,
+        skill: 'SAP AI Launchpad',
+        description: 'Build Enterprice-grade web apps with modern, rich user Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget feugiat nulla.'
+    },
+    {
+        id: 12,
+        skill: 'ODATA',
+        description: 'Build Enterprice-grade web apps with modern, rich user Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget feugiat nulla.'
+    },
+
+];
+
+const skills = (state = initialState, action) => { 
     switch (action.type) {
-        case 'SET_DETAILS':
-            return action.payload;
+        case 'GET_SKILLS':
+            return state;
         default:
             return state;
     }
